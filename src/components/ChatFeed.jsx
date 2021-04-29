985c7061d760cfe18ebfe67c5e21606aceb7fd08
Suman <sumanj631@gmail.com>
2021-04-29 18:09:19 +0530

    final

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -54,7 +54,7 @@ export default function ChatFeed(props) {
     <div className="chat-feed">
       <div className="chat-title-container">
         <div className="chat-title">{chat.title}</div>
-        <div className="chat-subtitle">{chat.people.map(person => `${person.person.username}`)},</div>
+        <div className="chat-subtitle">{chat.people.map(person => `${person.person.username},`)}</div>
       </div>
        {renderMessages()}
        <div style={{height: '100px'}}>
